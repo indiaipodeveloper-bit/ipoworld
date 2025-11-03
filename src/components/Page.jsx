bc1c3a4509442c9574d1a6f5f280664c51caf78a
IPO <indiaipodeveloper@gmail.com>
2025-11-03 10:19:49 +0530

    added

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -19,7 +19,6 @@ export default function Page({ title, children }) {
   ];
   const [activeNav, setActiveNav] = useState("library");
 
-
   const logout = () => {
     try {
       localStorage.removeItem("token");
@@ -52,83 +51,83 @@ export default function Page({ title, children }) {
 
   return (
     <div className="flex h-screen overflow-hidden">
-      
       {hasUser && (
-        <button
-          onClick={() => setSidebarOpen(true)}
-          className="lg:hidden cursor-pointer fixed top-5 left-2.5 z-50 w-11 h-11 bg-blue-500 rounded-xl shadow-lg hover:bg-blue-600 hover:scale-105 transition-all flex items-center justify-center"
-        >
-          <HiMenu className="w-5 h-5 text-white" />
-        </button>
-      )}
-       <div
-        className={`fixed lg:relative h-screen bg-white border-r border-gray-200 shadow-xl z-40 transition-all duration-300 flex flex-col ${
-          sidebarOpen ? "left-0 w-72" : "-left-72 w-0 lg:left-0 lg:w-72"
-        }`}
-      >
-        <div className="px-6 py-6 border-b border-gray-200">
+        <>
           <button
-            onClick={() => setSidebarOpen(false)}
-            className="absolute lg:hidden p-1  text-8xl cursor-pointer top-2 right-2 bg-blue-500 text-gray-50 rounded-lg font-medium hover:bg-blue-600 hover:-translate-y-0.5 hover:shadow-lg transition-all"
+            onClick={() => setSidebarOpen(true)}
+            className="lg:hidden cursor-pointer fixed top-5 left-2.5 z-50 w-11 h-11 bg-blue-500 rounded-xl shadow-lg hover:bg-blue-600 hover:scale-105 transition-all flex items-center justify-center"
           >
-            <HiX className="h-5 w-5" />
+            <HiMenu className="w-5 h-5 text-white" />
           </button>
-          <div className="flex items-center justify-start my-12 gap-3">
-            <img
-              src={indiaIPOLogo}
-              className="w-16 h-16 rounded-lg flex items-center justify-center text-white font-bold text-lg"
-            />
-            <span className="font-semibold text-gray-800">
-              India IPO Magazine
-            </span>
-          </div>
-        </div>
 
-        <nav className="flex-1 py-5 overflow-y-auto">
-          {navItems.map((item) => {
-            const Icon = item.icon;
-            return (
-              <div
-                key={item.id}
-                onClick={() => {
-                  setActiveNav(item.id);
-                }}
-                className={`flex items-center gap-3 px-6 py-3 cursor-pointer transition-all border-l-4 ${
-                  activeNav === item.id
-                    ? "bg-blue-50 text-blue-500 border-blue-500 font-medium"
-                    : "border-transparent text-gray-600 hover:bg-gray-50 hover:text-blue-500"
-                }`}
+          <div
+            className={`fixed lg:relative h-screen bg-white border-r border-gray-200 shadow-xl z-40 transition-all duration-300 flex flex-col ${
+              sidebarOpen ? "left-0 w-72" : "-left-72 w-0 lg:left-0 lg:w-72"
+            }`}
+          >
+            <div className="px-6 py-6 border-b border-gray-200">
+              <button
+                onClick={() => setSidebarOpen(false)}
+                className="absolute lg:hidden p-1  text-8xl cursor-pointer top-2 right-2 bg-blue-500 text-gray-50 rounded-lg font-medium hover:bg-blue-600 hover:-translate-y-0.5 hover:shadow-lg transition-all"
               >
-                <Icon className="w-5 h-5" />
-                <span>{item.label}</span>
+                <HiX className="h-5 w-5" />
+              </button>
+              <div className="flex items-center justify-start my-12 gap-3">
+                <img
+                  src={indiaIPOLogo}
+                  className="w-16 h-16 rounded-lg flex items-center justify-center text-white font-bold text-lg"
+                />
+                <span className="font-semibold text-gray-800">
+                  India IPO Magazine
+                </span>
               </div>
-            );
-          })}
-        </nav>
+            </div>
 
-        <div className="p-6 border-t border-gray-200">
-          <div className="bg-green-50 rounded-lg p-4 mb-4">
-            <h4 className="text-xs uppercase tracking-wide text-blue-900 mb-1">
-              Current Plan
-            </h4>
-            <p className="text-sm text-gray-700">Digital only (Monthly)</p>
+            <nav className="flex-1 py-5 overflow-y-auto">
+              {navItems.map((item) => {
+                const Icon = item.icon;
+                return (
+                  <div
+                    key={item.id}
+                    onClick={() => {
+                      setActiveNav(item.id);
+                    }}
+                    className={`flex items-center gap-3 px-6 py-3 cursor-pointer transition-all border-l-4 ${
+                      activeNav === item.id
+                        ? "bg-blue-50 text-blue-500 border-blue-500 font-medium"
+                        : "border-transparent text-gray-600 hover:bg-gray-50 hover:text-blue-500"
+                    }`}
+                  >
+                    <Icon className="w-5 h-5" />
+                    <span>{item.label}</span>
+                  </div>
+                );
+              })}
+            </nav>
+
+            <div className="p-6 border-t border-gray-200">
+              <div className="bg-green-50 rounded-lg p-4 mb-4">
+                <h4 className="text-xs uppercase tracking-wide text-blue-900 mb-1">
+                  Current Plan
+                </h4>
+                <p className="text-sm text-gray-700">Digital only (Monthly)</p>
+              </div>
+              <button
+                onClick={logout}
+                className="w-full flex items-center cursor-pointer justify-center gap-2 px-4 py-3 bg-blue-500 text-white rounded-lg font-medium hover:bg-blue-600 hover:-translate-y-0.5 hover:shadow-lg transition-all"
+              >
+                <IoLogOutOutline className="w-4 h-4" />
+                Logout
+              </button>
+            </div>
           </div>
-          <button
-            onClick={logout}
-            className="w-full flex items-center cursor-pointer justify-center gap-2 px-4 py-3 bg-blue-500 text-white rounded-lg font-medium hover:bg-blue-600 hover:-translate-y-0.5 hover:shadow-lg transition-all"
-          >
-            <IoLogOutOutline className="w-4 h-4" />
-            Logout
-          </button>
-        </div>
-      </div>
+        </>
+      )}
 
       <main className="flex-1 overflow-y-auto py-8">
         {title && <h2 className="mb-4 text-2xl font-semibold">{title}</h2>}
         {children}
       </main>
     </div>
-
-    
   );
 }
