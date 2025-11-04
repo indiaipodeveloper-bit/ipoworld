00267ae3bf4127a69bb1a68a6b07865192fbb605
IPO <indiaipodeveloper@gmail.com>
2025-11-04 09:59:31 +0530

    adsf

--- src/pages/prelogin/PdfViewer.jsx
+++ src/pages/prelogin/PdfViewer.jsx
@@ -255,7 +255,7 @@ export default function PdfViewer({ url }) {
             <div
               className={`transition-opacity duration-700 ease-in-out ${
                 pageNum <= 1 ? "opacity-100 flex" : "opacity-0 hidden"
-              } text-white bg-black/70 transition-all duration-300 text-lg lg:text-xl text-center top-1/4 w-1/3 z-50 my-5 mx-auto fixed font-extrabold gap-x-10 justify-center items-center rounded-lg backdrop-blur-sm p-4`}
+              } text-white bg-black/70 transition-all duration-300 text-lg lg:text-xl text-center top-1/4 w-1/3 z-50 my-5  fixed font-extrabold gap-x-10 justify-center items-center rounded-lg backdrop-blur-sm p-4`}
             >
               <FaLongArrowAltLeft />
               <p>Click or Swipe to Read</p>
@@ -284,7 +284,7 @@ export default function PdfViewer({ url }) {
                       {imageData ? (
                         <div
                           style={{ backgroundImage: `url(${imageData})` }}
-                          className="h-full w-full mx-auto bg-no-repeat m-auto bg-center overflow-auto bg-contain object-cover"
+                          className="h-full w-full bg-no-repeat m-auto bg-center overflow-auto bg-contain object-cover"
                         ></div>
                       ) : (
                         <div className="w-full h-full flex items-center justify-center bg-gray-100">
