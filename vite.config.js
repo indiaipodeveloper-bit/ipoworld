06eb8a3a46f3df352275c69d1347c7d8e2333275
IPO <indiaipodeveloper@gmail.com>
2025-11-11 09:53:57 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
-    host: "0.0.0.0", // listen on all network interfaces
-    // port: 5173, // optional: choose a fixed port
+    host: "0.0.0.0",
   },
   plugins: [react()],
 });
